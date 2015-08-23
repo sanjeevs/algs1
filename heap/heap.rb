9679d6c82d9512a907f0274d721510c2cc45609a
sanjeev singh <spsingh2@ncsu.edu>
2015-08-23 18:38:51 -0400

    passing tests

--- heap/heap.rb
+++ heap/heap.rb
@@ -1,25 +1,80 @@
 class Heap
-  def initilaize
+  attr_reader :arr
+  attr_writer :arr
+  def initialize
     @arr = []
   end
 
-  def parent(index)
-    raise IndexError unless index < @arr.size
-    (index/2.0).ceil - 1
+  def root
+    return nil if @arr.empty?
+    @arr[0]
   end
 
-  def children(index)
+  def parent_index(index)
+    (index/2.0).ceil - 1
+  end
+  
+  def parent(index)
+    raise IndexError if index >= @arr.size
+    i = parent_index(index)
+    return nil if i >= @arr.size
+    @arr[i]
+  end
+
+  def left_index(index)
+    2 * (index + 1) - 1
   end
 
   def left_child(index)
+    i = left_index(index)
+    return nil if i >= @arr.size
+    return @arr[i]
+  end
+
+  def right_index(index)
+    2 * (index + 1)
   end
 
   def right_child(index)
+    i = right_index(index)
+    return nil if i >= @arr.size
+    return @arr[i]
+  end
+
+  
+  def node(index)
+    @arr[index]
+  end
+
+  def swap_index(i1, i2)
+    temp = @arr[i1]
+    @arr[i1] = @arr[i2]
+    @arr[i2] = temp
+  end
+
+  def hash_swap(index)
+    puts "hash_swap at index=#{index}"
+    parent = @arr[index]
+    if left_child(index)
+      if right_child(index)
+        if left_child(index) < right_child(index) and parent > left_child(index)
+          swap_index(left_index(index), index)
+          return left_index(index)
+        elsif right_child(index) < left_child(index) and parent > right_child(index)
+          swap_index(right_index(index), index)
+          return right_index(index)
+        end
+      elsif parent > left_child(index)
+        swap_index(left_index(index), index)
+        return left_index(index)
+      end
+    end
+    return -1
   end
 
   def insert(node)
     @arr << node
-    bubble_up
+    #bubble_up
   end
 
   def extract_min
@@ -28,11 +83,44 @@ class Heap
     bubble_down
   end
 
-  def bubble_up
+  def bubble_up(i)
+    return nil if i == 0 
+    index = @arr[parent_index(i)]
+    parent = @arr[index]
+    if left_child(index)
+      if right_child(index)
+        if left_child(index) < right_child(index) and parent > left_child(index)
+          swap_index(left_index(index), index)
+          bubble_up(index)
+        elsif right_child(index) < left_child(index) and parent > right_child(index)
+          swap_index(right_index(index), index)
+          bubble_up(index)
+        end
+      elsif parent > left_child(index)
+        swap_index(left_index(index), index)
+        bubble_up(index)
+      end
+    end
   end
 
-  def bubble_down
+  def bubble_down(index)
+    index = hash_swap(index) until index == -1 
   end
+
+
+  def sanity_check
+    @arr.each_index do |i|
+      #puts "Matching index=#{i} parent=#{@arr[i]} with left_child=#{left_child(i)} and right_child=#{right_child(i)}"
+      if left_child(i)
+        return false unless left_child(i) > @arr[i]
+      end
+      if right_child(i)
+        return false unless right_child(i) > @arr[i]
+      end
+    end
+    return true
+  end
+
 end
 
 
--- heap/test_heap.rb
+++ heap/test_heap.rb
@@ -1,5 +1,5 @@
 require 'test/unit'
-
+require_relative 'heap.rb'
 class TestHeap < Test::Unit::TestCase
   def setup
     @heap = Heap.new
@@ -8,29 +8,57 @@ class TestHeap < Test::Unit::TestCase
     end
   end
 
+  def test_sanity
+    assert_equal true, @heap.sanity_check
+  end
+
   def test_root
     assert_equal 0, @heap.root
   end
 
   def test_parent
-    assert_equal nil, @heap.parent(0)
     (1..2).each {|x| assert_equal 0, @heap.parent(x) }
     (3..4).each {|x| assert_equal 1, @heap.parent(x) }
     (13..14).each {|x| assert_equal 6, @heap.parent(x) }
   end
 
   def test_children
-    assert_equal [1,2], @heap.children(0)
     assert_equal 1, @heap.left_child(0)
     assert_equal 2, @heap.right_child(0)
-    assert_equal [13,14], @heap.children(6)
-    assert_equal [7,8], @heap.children(3)
+    assert_equal 13, @heap.left_child(6)
+    assert_equal 14, @heap.right_child(6)
+    assert_equal 7, @heap.left_child(3)
+    assert_equal 8, @heap.right_child(3)
   end
- 
+
+  def test_swap
+    @heap.swap_index(1, 3)
+    assert_equal 3, @heap.node(1) 
+    assert_equal 1, @heap.node(3)
+  end
+
   def test_invalid_access
     assert_raise IndexError do
       @heap.parent(16)
     end
+  end
+  
+  def test_bubble_down1
+    heap1 = Heap.new
+    heap1.insert(16)
+    (0..15).each do |i|
+      heap1.insert(i)
+    end
+    heap1.bubble_down(0)
+    assert_equal [0,2,1,6,3,4,5,14,7,8,9,10,11,12,13,16,15], heap1.arr
+  end
+  
+  def test_bubble_up1
+    heap1 = @heap
+    heap1.arr << 0
+    puts heap1.arr.to_s
+    heap1.bubble_up(15)
+    assert_equal [0,0,2,1,4,5,6,3,8,9,10,11,12,13,14,15,7], heap1.arr
   end
 
 end
